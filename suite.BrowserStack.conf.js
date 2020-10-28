8c1a8e94a04885e33e9b802276d1d7c5634f626b
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-10-28 16:23:44 -0400

    local run configuration

--- suite.BrowserStack.conf.js
+++ suite.BrowserStack.conf.js
@@ -1,30 +1,8 @@
-/*
-var userName = "andreikalianchuk1";
-var accessKey = "1iad6xYJqPxo46qUe6ri"
-var browserstackURL = 'https://' + userName + ':' + accessKey + '@hub-cloud.browserstack.com/wd/hub';
-*/
-
 exports.config = {
-    //Browser Stack configuration
-    user: process.env.BROWERSTACK_USERNAME, //'andreikalianchuk1',
-    key: process.env.BROWSERSTACK_KEY, // '1iad6xYJqPxo46qUe6ri'
-
-    // ====================
-    // Runner Configuration
-    // ====================
-    //
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
+    user: process.env.BROWERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_KEY,
     runner: 'local',
-    //
-    // ==================
-    // Specify Test Files
-    // ==================
-    // Define which test specs should run. The pattern is relative to the directory
-    // from which `wdio` was called. Notice that, if you are calling `wdio` from an
-    // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
-    // directory is where your package.json resides, so `wdio` will be called from there.
-    //
+
     specs: [
         './features/**/login.feature'
     ],
@@ -32,29 +10,10 @@ exports.config = {
     exclude: [
         // 'path/to/excluded/files'
     ],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
-    // time. Depending on the number of capabilities, WebdriverIO launches several test
-    // sessions. Within your capabilities you can overwrite the spec and exclude options in
-    // order to group specific specs to a specific capability.
-    //
-    // First, you can define how many instances should be started at the same time. Let's
-    // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-    // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-    // files and you set maxInstances to 10, all spec files will get tested at the same time
-    // and 30 processes will get spawned. The property handles how many capabilities
-    // from the same test should run tests.
-    //
+
     maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://docs.saucelabs.com/reference/platforms-configurator
-    //
-    capabilities: [{
+
+    capabilities: [ {
         os: 'Windows',
         os_version: '10',
         browserName: 'chrome',
@@ -74,46 +33,20 @@ exports.config = {
             browserName: 'safari',
             browserVersion : "13.0",
             browserstackLocal: true
-        }],
+        }
+        ],
 
-    //
-    // ===================
-    // Test Configurations
-    // ===================
-    // Define all options that are relevant for the WebdriverIO instance here
-    //
+
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'silent',
-    //
-    // Set specific log levels per logger
-    // loggers:
-    // - webdriver, webdriverio
-    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
-    // - @wdio/mocha-framework, @wdio/jasmine-framework
-    // - @wdio/local-runner
-    // - @wdio/sumologic-reporter
-    // - @wdio/cli, @wdio/config, @wdio/sync, @wdio/utils
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    // logLevels: {
-    //     webdriver: 'info',
-    //     '@wdio/applitools-service': 'info'
-    // },
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
+    logLevel: 'info',
+
     bail: 0,
-    //
-    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-    // gets prepended directly.
+
     baseUrl: 'http://stage.localcoding.us/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
-    //
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
+
     connectionRetryTimeout: 120000,
     //
     // Default request retries count
@@ -124,17 +57,12 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        ['browserstack',  {
-        browserstackLocal: true
-    }]
+        ['browserstack', {
+         //   browserstackLocal: true
+        }]
     ],
 
-    // Framework you want to run your specs with.
-    // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: https://webdriver.io/docs/frameworks.html
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed
-    // before running any tests.
+
     framework: 'cucumber',
     //
     // The number of times to retry the entire specfile when it fails as a whole
